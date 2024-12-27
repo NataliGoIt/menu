bdbbd64edd8631ccad63fb15cd6a3c1def021f08
Natali <91018314+NataliGoIt@users.noreply.github.com>
2024-12-27 14:05:46 +0100

    products/img

---
+++ products.js
@@ -0,0 +1,190 @@
+// import photo from "../img/pizza.png"
+export const pizza = [
+    {
+      id: 1,
+      title: "PIZZA CEZAR",
+      description: "Aluat, sos, mozzarella, cascaval, pui, becon, verdeturi, porumb, cherry rosi, permezan",
+      sizes: [
+        { size: "32cm - 710gr", price: "35 lei", title: "Standart"},
+        { size: "32cm - 980gr", price: "50 lei", title: "Hrănitoare" },
+        { size: "40cm - 1440gr", price: "65 lei", title: "Mare" }
+      ],
+      price: 19.99,
+      image: "https://th.bing.com/th/id/R.66acbd1dd5b8de9cb44f24a790621c6a?rik=ShYP1mb0ht2IPg&pid=ImgRaw&r=0",
+    },
+    {
+      id: 2,
+      title: "PIZZA CU LEGUME",
+      description: "Aluat, sos, mozzarella, cascaval, roși, ardei, verdeturi, dor blu, ciuperci",
+      sizes: [
+        { size: "32cm - 710gr", price: "30 lei", title: "Standart"},
+        { size: "32cm - 860gr", price: "40 lei", title: "Hrănitoare" },
+        { size: "40cm - 1270gr", price: "55 lei", title: "Mare" }
+      ],
+      image: "https://th.bing.com/th/id/R.66acbd1dd5b8de9cb44f24a790621c6a?rik=ShYP1mb0ht2IPg&pid=ImgRaw&r=0",
+    },
+    {
+      id: 3,
+      title: "PIZZA DIABLO",
+      description: "Aluat, sos, mozzarella, chorizo, prosciutto, parmezan, ardei verde, ardei iute",
+      sizes: [
+        { size: "32cm - 650gr", price: "35 lei", title: "Standart"},
+        { size: "32cm - 890gr", price: "45 lei", title: "Hrănitoare" },
+        { size: "40cm - 1220gr", price: "60 lei", title: "Mare" }
+      ],
+      image: "https://th.bing.com/th/id/R.66acbd1dd5b8de9cb44f24a790621c6a?rik=ShYP1mb0ht2IPg&pid=ImgRaw&r=0",
+    },
+    {
+      id: 4,
+      title: "PIZZA MARGHERITA",
+      description: "Aluat, sos, mozzarella, roși, tomato pelati, busuios",
+      sizes: [
+        { size: "32cm - 750gr", price: "30 lei", title: "Standart"},
+        { size: "32cm - 1020gr", price: "43 lei", title: "Hrănitoare" },
+        { size: "40cm - 1310gr", price: "60 lei", title: "Mare" }
+      ],
+      image: "https://th.bing.com/th/id/R.66acbd1dd5b8de9cb44f24a790621c6a?rik=ShYP1mb0ht2IPg&pid=ImgRaw&r=0",
+    },
+    {
+      id: 5,
+      title: "PIZZA PEPPERONI",
+      description: "Aluat, sos, tomato pelati, oregano, mozzarella, chorizo, cascaval, ardei iute",
+      sizes: [
+        { size: "32cm - 770gr", price: "35 lei", title: "Standart"},
+        { size: "32cm - 1020gr", price: "45 lei", title: "Hrănitoare" },
+        { size: "40cm - 1340gr", price: "60 lei", title: "Mare" }
+      ],
+      image: "https://th.bing.com/th/id/R.66acbd1dd5b8de9cb44f24a790621c6a?rik=ShYP1mb0ht2IPg&pid=ImgRaw&r=0",
+    },
+    {
+      id: 6,
+      title: "PIZZA CU PUI ȘI ANANAS",
+      description: "Aluat, sos, mozzarella, cascaval, pui, becon, ananas, porumb, parmezan",
+      sizes: [
+        { size: "32cm - 760gr", price: "35 lei", title: "Standart"},
+        { size: "32cm - 1080gr", price: "45 lei", title: "Hrănitoare" },
+        { size: "40cm - 1390gr", price: "65 lei", title: "Mare" }
+      ],
+      image: "https://th.bing.com/th/id/R.66acbd1dd5b8de9cb44f24a790621c6a?rik=ShYP1mb0ht2IPg&pid=ImgRaw&r=0",
+    },
+    {
+      id: 7,
+      title: "PIZZA QUATTRO FORMAGGI",
+      description: "Aluat, mozzarella, cascaval, parmezan, „dor blue”, sos",
+      sizes: [
+        { size: "32cm - 640gr", price: "35 lei", title: "Standart"},
+        { size: "32cm - 860gr", price: "45 lei", title: "Hrănitoare" },
+        { size: "40cm - 1260gr", price: "65 lei", title: "Mare" }
+      ],
+      image: "https://th.bing.com/th/id/R.66acbd1dd5b8de9cb44f24a790621c6a?rik=ShYP1mb0ht2IPg&pid=ImgRaw&r=0",
+    },
+    {
+      id: 8,
+      title: "PIZZA CU ȘUNCA",
+      description: "Aluat, șunca, ardei dulce, brănză, sos",
+      sizes: [
+        { size: "32cm - 650gr", price: "30 lei", title: "Standart"},
+        { size: "32cm - 980gr", price: "43 lei", title: "Hrănitoare" },
+        { size: "40cm - 1450gr", price: "60 lei", title: "Mare" }
+      ],
+      image: "https://th.bing.com/th/id/R.66acbd1dd5b8de9cb44f24a790621c6a?rik=ShYP1mb0ht2IPg&pid=ImgRaw&r=0",
+    },
+    {
+      id: 9,
+      title: "PIZZA BAVAREZA",
+      description: "Aluat, salami, cărnati, mozzarella, parmezan, roși, ciuperci, măsline, sos",
+      sizes: [
+        { size: "32cm - 730gr", price: "35 lei", title: "Standart"},
+        { size: "32cm - 1040gr", price: "50 lei", title: "Hrănitoare" },
+        { size: "40cm - 1610gr", price: "65 lei", title: "Mare" }
+      ],
+      image: "https://th.bing.com/th/id/R.66acbd1dd5b8de9cb44f24a790621c6a?rik=ShYP1mb0ht2IPg&pid=ImgRaw&r=0",
+    },
+    // Додай інші продукти за потреби
+  ];
+
+  export const drink = [
+  {
+    id: 1,
+    title: "Coca-cola",
+    description: "0,51",
+    price: "9 lei",
+    image: "#",
+  },
+  {
+    id: 2,
+    title: "Coca-cola",
+    description: "0,331",
+    price: "7 lei",
+    image: "#",
+  },
+  {
+    id: 3,
+    title: "Coca-cola zero zahar",
+    description: "0,331",
+    price: "7 lei",
+    image: "#",
+  },
+  {
+    id: 4,
+    title: "Fanta la doza",
+    description: "0,331",
+    price: "7 lei",
+    image: "#",
+  },
+  {
+    id: 5,
+    title: "Fanta madness",
+    description: "0,331",
+    price: "7 lei",
+    image: "#",
+  },
+  {
+    id: 6,
+    title: "Suc",
+    description: "0,21",
+    price: "5 lei",
+    image: "#",
+  },
+  {
+    id: 7,
+    title: "Suc",
+    description: "0,41",
+    price: "8 lei",
+    image: "#",
+  },
+  {
+    id: 8,
+    title: "Borsec apa plata",
+    description: "0,51",
+    price: "5 lei",
+    image: "#",
+  },
+  {
+    id: 9,
+    title: "Borsec apa minerala",
+    description: "0,51",
+    price: "5 lei",
+    image: "#",
+  },
+  ];
+  // export const otherProducts = [
+  //   {
+  //     id: 1,
+  //     title: "",
+  //     description: "",
+  //     price: 12,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "",
+  //     description: "",
+  //     price: 12,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "",
+  //     description: "",
+  //     price: 12,
+  //   },
+  // ]

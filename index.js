a12a90eec9528a6527a1723387550564df24a3c4
Natali <91018314+NataliGoIt@users.noreply.github.com>
2025-01-02 18:45:04 +0100

    modal-window

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const languageBtn = document.querySelector(".language-btn");
-const languageDropdown = document.querySelector(".language-dropdown");
-const selector = document.querySelector('.language-selector');
+const languageBtn = document.querySelector(".language_btn");
+const languageDropdown = document.querySelector(".language_dropdown");
+const selector = document.querySelector('.language_selector');
+const openModalBtn = document.querySelector('.modal-window_open');
+const closeModalBtn = document.querySelector('.modal-window_close');
+const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('.modal-window');
+  
+    openModalBtn.addEventListener('click', toggleModal);
+    closeModalBtn.addEventListener('click', toggleModal);
+    backdrop.addEventListener('click',(event) => (!modal.contains(event.target) && toggleModal()));
+   
+  function toggleModal() {
+    backdrop.classList.toggle('is-hidden');
+  }
 document.addEventListener("DOMContentLoaded", () => {
   
     languageDropdown.addEventListener("click", (event) => {
@@ -12,7 +24,6 @@ document.addEventListener("DOMContentLoaded", () => {
         // Змінюємо прапорець і текст кнопки
         languageBtn.innerHTML = `
           <img src="${selectedFlag}" alt="${selectedText}" class="flag-icon" />
-          ${selectedText}
         `;
   
         if (selector.contains(event.target)) {

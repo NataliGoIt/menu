334397b8ccbea86a59dfb0e381c396593e7ac438
Natali <91018314+NataliGoIt@users.noreply.github.com>
2025-01-04 11:08:55 +0100

    menu

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const openModalBtn = document.querySelector('.modal-window_open');
 const closeModalBtn = document.querySelector('.modal-window_close');
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal-window');
+const pizzaItems = document.querySelectorAll('.pizza_item');
   
     openModalBtn.addEventListener('click', toggleModal);
     closeModalBtn.addEventListener('click', toggleModal);
@@ -21,7 +22,6 @@ document.addEventListener("DOMContentLoaded", () => {
         const selectedFlag = event.target.dataset.flag;
         const selectedText = event.target.textContent;
   
-        // Змінюємо прапорець і текст кнопки
         languageBtn.innerHTML = `
           <img src="${selectedFlag}" alt="${selectedText}" class="flag-icon" />
         `;
@@ -37,4 +37,23 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
   
+
+
+
+pizzaItems.forEach((item) => {
   
+  item.addEventListener('click', () => {
+    const details = item.querySelector('.item_details');
+    const isVisible = details.classList.contains('is-visible');
+
+    pizzaItems.forEach((otherItem) => {
+      otherItem.querySelector('.pizza_item-main').classList.remove('hide');
+      otherItem.querySelector('.item_details').classList.remove('is-visible');
+    });
+
+    if (!isVisible) {
+      item.querySelector('.pizza_item-main').classList.add('hide');
+      details.classList.add('is-visible');
+    }
+  });
+});
